2171c3732bc06b61a27c5b69354a6d5a869a4b32
Anthony Cramp <anthony.cramp@gmail.com>
2023-10-22 11:33:15 +0000

    Reformated to capture the equation as a domain concept

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use rand::prelude::*;
+use std::fmt;
 
 pub fn greeting(name: &str) -> String {
     format!("Hello, {}", name)
@@ -9,7 +10,41 @@ pub fn get_operand(min: i32, max: i32) -> i32 {
     rng.gen_range(min..=max)
 }
 
-pub fn get_addition_equation(min_operand: i32, target_sum: i32) -> String {
+pub enum Operator {
+    Addition,
+}
+
+impl fmt::Display for Operator {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", match self {
+            Operator::Addition => "+",
+        })
+    }
+}
+
+pub struct Equation {
+    pub first_operand: i32,
+    pub second_operand: i32,
+    pub operator: Operator,
+}
+
+impl Equation {
+    fn new(first_operand: i32, second_operand: i32, operator: Operator) -> Self {
+        Self {
+            first_operand,
+            second_operand,
+            operator,
+        }
+    }
+}
+
+impl fmt::Display for Equation {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} {} {} =", self.first_operand, self.operator, self.second_operand)
+    }
+}
+
+pub fn create_addition_equation(min_operand: i32, target_sum: i32) -> Equation {
     let mut rng = thread_rng();
     let upper_bound = std::cmp::min(10, target_sum);
     let mut first_operand = rng.gen_range(min_operand..upper_bound);
@@ -18,13 +53,23 @@ pub fn get_addition_equation(min_operand: i32, target_sum: i32) -> String {
     }
     let second_operand = target_sum - first_operand;
 
-    format!("{} + {} =", first_operand, second_operand)
+    Equation::new(first_operand, second_operand, Operator::Addition)
 }
 
-pub fn create_addition_list(number_of_entries: i32, min_sum: i32, max_sum: i32) -> Vec<String> {
+pub fn create_addition_list(number_of_entries: i32, min_sum: i32, max_sum: i32) -> Vec<Equation> {
     let mut addition_list = vec![]; 
     for target_sum in min_sum..=max_sum {
-        addition_list.push(get_addition_equation(2, target_sum));
+        addition_list.push(create_addition_equation(2, target_sum));
     }
     addition_list
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create_addition_equation() {
+
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use math_sheets::*;
 fn main() {
     println!("{}", greeting("Yasmin"));
     println!("{}", get_operand(9,10));
-    println!("{}", get_addition_equation(2,12));
+    println!("{}", create_addition_equation(2,12));
 
     let addition_list = create_addition_list(20,3,18);
     for addition_equation in addition_list {
