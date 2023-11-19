3b0bd7dddfd9c4c619867e7e55a0862e2554924c
Anthony Cramp <anthony.cramp@gmail.com>
2023-11-19 21:35:32 +1030

    Remove some debugging output from main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 use math_sheets::*;
 
 fn main() {
-    println!("{}", greeting("Yasmin"));
-    println!("{}", get_operand(9, 10));
-    println!("{}", create_addition_equation(2, 12));
-
     let addition_list = create_addition_list(25, 3, 18);
     for addition_equation in addition_list {
         println!("{}", addition_equation);
