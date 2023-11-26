1be38576f3cef48a642a4a7f9e58a75b4fdb1d97
Anthony Cramp <anthony.cramp@gmail.com>
2023-11-26 19:54:47 +1030

    Modify CSS a bit

--- src/main.rs
+++ src/main.rs
@@ -8,36 +8,38 @@ fn main() {
     body {{
         align: center;
     }}
-    .equation {{
-        pad: 10px;
-        margin: 10px;
-        align: center;
+    .equations {{
+        display: grid;
+        gap: 10px;
     }}
-    .operand {{
-        margin: 5px;
+    .equation {{
+        display: flex;
+    }}
+    .operand-one {{
+        margin: 1px;
+    }}
+    .operand-two {{
+        margin: 1px;
     }}
     .operator {{
         margin: 1px;
     }}
     .answer {{
-        width: 50px;
-        height: 50px;
-        padding: 1px;
-        marging: 5px;
         border: thin solid black;
     }}
     </style>
     </head>
-    <body>"#
+    <body>
+    <div class="equations">"#
     );
     let addition_list = create_addition_list(20, 3, 18);
     for addition_equation in addition_list {
         println!(
-            r#"<div class="equation"><span class="operand">{}</span><span class="operator">{}</span><span class="operand">{}</span><span class="equal-sign">=</span><span class="answer">&emsp;&emsp;</span></div>"#,
+            r#"<div class="equation"><span class="operand-one">{}</span><span class="operator">{}</span><span class="operand-two">{}</span><span class="equal-sign">=</span><span class="answer">&emsp;&emsp;</span></div>"#,
             addition_equation.first_operand,
             addition_equation.operator,
             addition_equation.second_operand,
         );
     }
-    println!("</body></html>")
+    println!("</div></body></html>")
 }
